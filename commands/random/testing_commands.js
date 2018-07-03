38d5089eb3b6e2c7fb3ab4f66aa5af0b0f8ca2f0
saul <saul.reyna90@gmail.com>
2018-07-02 19:22:04 -0500

    updated package.json

--- commands/random/testing_commands.js
+++ commands/random/testing_commands.js
@@ -8,6 +8,9 @@ var pers_mess = fs.readFileSync('C:\\Users\\saulr\\Documents\\GitHub\\saulbot\\c
 var messagesObj = [];
 messagesObj = JSON.parse(pers_mess);
 
+const RapidAPI = new require('rapidapi-connect');
+const rapid = new RapidAPI('default-application_5b38eb08e4b0d9d6841d1926', '2091d3ee-69f1-425c-9033-3a0831bc7ee1');
+
 class TestingCommands extends commando.Command {
     constructor(client) {
         super(client, {
@@ -19,38 +22,15 @@ class TestingCommands extends commando.Command {
     }
 
     async run(message, args) { // jshint ignore:line
-        // console.log(message.mentions);
-        function isEmpty(obj) {
-            for (var key in obj) {
-                if (obj.hasOwnProperty(key))
-                    return false;
-            }
-            return true;
-        }
+        
+        unirest.get("https://montanaflynn-fifa-world-cup.p.mashape.com/goals")
+        .header("accepts", "json")
+        .header("X-Mashape-Key", "puwX6kaVukmshqzNwzN811tn94MPp1LDaCajsnixmn0R4lTjHC")
+        .header("X-Mashape-Host", "montanaflynn-fifa-world-cup.p.mashape.com")
+        .end(function (result) {
+        console.log(result.status, result.headers, result.body);
+        });
 
-        function countKeys(obj) {
-            var counter = 0;
-            for (var key in obj) {
-                if (obj.hasOwnProperty(key))
-                    counter += counter;
-            }
-            return counter;
-        }
-
-
-        // if ("141255836914679808" in messagesObj && "14125583691467980" in messagesObj) {
-        //    console.log('key in object');
-        //} else console.log('key nonexisten');
-
-        /* 
-        if (isEmpty(message.mentions.members.keyArray())) {
-             console.log('array empty')
-         } else console.log('array not empty');
-         */
-
-        console.log('message: ' + message.content + ' mention: ' + message.mentions.members.keyArray()[0]);
-        message.channel.send('<:pingsocker:461918432770785292>');
-        message.channel.send(':weary:');
     }
 }
 
