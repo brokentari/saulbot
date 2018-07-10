348b776f39ed706a7a18bd355cec50232a1f35d6
saul <saul.reyna90@gmail.com>
2018-07-10 07:49:56 -0500

    added ping command

---
+++ app.js
@@ -0,0 +1,17 @@
+var Ping = require('./lib/ping'),
+websites = [
+    {
+        url: 'http://saulbot.herokuapp.com',
+        timeout: 5
+    }
+],
+monitors = [];
+
+websites.forEach(function (website) {
+var monitor = new Ping ({
+    website: website.url,
+    timeout: website.timeout
+});
+
+monitors.push(monitor);
+});
