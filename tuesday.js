dc2f8c7b16c442ff8aa64e669f70ccff5167a2d2
saul <saul.reyna90@gmail.com>
2018-08-02 17:45:54 -0500

    fixed tuesday method

--- tuesday.js
+++ tuesday.js
@@ -1,29 +1,31 @@
 /*jshint esversion: 6 */
 
-const Discord = require('discord.js');
-const schedule = require('node-schedule');
+const Discord = require("discord.js");
+const schedule = require("node-schedule");
 const client = new Discord.Client();
 
-client.login(objsArray.token).then(() => {
-    console.log('im ready');
-    var date = new Date();
+client.login(process.env.LOGINTK).then(() => {
+  console.log("im ready");
+  var date = new Date();
 
-    console.log(date.getDay());
-    if (date.getDay() == 2) {
-        var guild = client.guilds.get('432343677759651841');
-        if (guild && guild.channels.get('432343677759651843')) {
-            guild.channels.get('432343677759651843').send({
-                files: [{
-                    attachment: 'tuesday.png',
-                    name: 'tuesday.png'
-                }]
-            });
-        } else {
-            console.log('nope');
-        }
+  console.log(date.getDay());
+  if (date.getDay() == 2) {
+    var guild = client.guilds.get("432343677759651841");
+    if (guild && guild.channels.get("432343677759651843")) {
+      guild.channels.get("432343677759651843").send({
+        files: [
+          {
+            attachment: "tuesday.png",
+            name: "tuesday.png"
+          }
+        ]
+      });
     } else {
-        console.log('its not tuesday yet');
+      console.log("nope");
     }
-});
+  } else {
+    console.log("its not tuesday yet");
+  }
 
-process.exit();
+  process.exit(22);
+});
